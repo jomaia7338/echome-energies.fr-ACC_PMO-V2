14409442c0aa819a55749f7d4a142c4c4279c8f6
jomaia7338 <jomaia7338@gmail.com>
2025-09-29 17:51:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,23 @@
 const CACHE_NAME = 'echome-acc-terrain-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './','./index.html','./styles.css','./app.js','./manifest.webmanifest',
+  './icons/icon-192.png','./icons/icon-512.png',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
-
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
 });
-
 self.addEventListener('fetch', (e) => {
-  const { request } = e;
-  if (request.method !== 'GET') return;
+  const { request } = e; if (request.method !== 'GET') return;
   e.respondWith(
     caches.match(request).then(cached =>
-      cached ||
-      fetch(request).then((resp) => {
+      cached || fetch(request).then((resp) => {
         const copy = resp.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(request, copy)).catch(()=>{});
         return resp;
